d4fccab265b222829134d01bb78ca043aab3e577
Aelf86 <139387685+Aelf86@users.noreply.github.com>
2024-03-08 23:35:00 +0000

    mis ccambios

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -6,18 +6,18 @@ import React from "react";
 //create your first component
 const Navbar = () => {
 	return (
-		<nav class="navbar navbar-expand-lg bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+		<nav className="navbar navbar-dark navbar-expand-lg bg-dark">
+  <div className="container d-flex justify-content-between w-100">
+    <a className="navbar-brand" href="#">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
-        <a class="nav-link" href="#">Features</a>
-        <a class="nav-link" href="#">Pricing</a>
-        <a class="nav-link disabled">Disabled</a>
+    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div className="navbar-nav ms-auto">
+        <a className="nav-link active" aria-current="page" href="#">Home</a>
+        <a className="nav-link" href="#">Features</a>
+        <a className="nav-link" href="#">Pricing</a>
+        <a className="nav-link disabled">Disabled</a>
       </div>
     </div>
   </div>
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,14 +4,31 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import Navbar from "./Navbar.jsx";
 import Jumbotron from "./Jumbotron.jsx";
+import Card from "./Card.jsx";
 
 //create your first component
 const Home = () => {
+	const cardImages = ["https://images.hola.com/imagenes/viajes/20220930218161/salto-angel-venezuela-catarata-mas-alta-mundo/1-145-411/salto-del-angel-canaima-venezuela-t.jpg",
+	"https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1b/01/6b/6f/caption.jpg?w=1200&h=-1&s=1",
+	"https://deguantasoy.com/wp-content/uploads/2023/05/patanemo.jpg",
+	"https://cdn0.geoenciclopedia.com/es/posts/7/8/6/pico_bolivar_687_orig.jpg"]
 	return (
-		<div className = "text-center">
+<>               
 			<Navbar/>
+		<div className = "container">
 			<Jumbotron/>
+			
+			<div className="d-flex">
+			{cardImages.map((item,index) => {
+				return(
+					<Card key={index} img={item}/>
+				)
+			})}
+			</div>
+			
 		</div>
+
+	</>
 	);
 };
 
