20ee11dd2cc06a2df49f2c7d2dff8a3fca997ab6
Aelf86 <139387685+Aelf86@users.noreply.github.com>
2024-03-11 21:56:59 +0000

    listo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,27 +8,41 @@ import Card from "./Card.jsx";
 import Footer from "./Footer.jsx";
 //create your first component
 const Home = () => {
-	const cardImages = ["https://images.hola.com/imagenes/viajes/20220930218161/salto-angel-venezuela-catarata-mas-alta-mundo/1-145-411/salto-del-angel-canaima-venezuela-t.jpg",
-	"https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1b/01/6b/6f/caption.jpg?w=1200&h=-1&s=1",
-	"https://deguantasoy.com/wp-content/uploads/2023/05/patanemo.jpg",
-	"https://cdn0.geoenciclopedia.com/es/posts/7/8/6/pico_bolivar_687_orig.jpg"]
+	const cardImages = [
+		{
+			"imagen": "https://images.hola.com/imagenes/viajes/20220930218161/salto-angel-venezuela-catarata-mas-alta-mundo/1-145-411/salto-del-angel-canaima-venezuela-t.jpg",
+			"titulo": "Salto Ángel"
+		},
+		{
+			"imagen": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1b/01/6b/6f/caption.jpg?w=1200&h=-1&s=1",
+			"titulo": "Choroní"
+		},
+		{
+			"imagen": "https://deguantasoy.com/wp-content/uploads/2023/05/patanemo.jpg",
+			"titulo": "Bahía de Patanemo"
+		},
+		{
+			"imagen": "https://cdn0.geoenciclopedia.com/es/posts/7/8/6/pico_bolivar_687_orig.jpg",
+			"titulo": "Pico Bolívar"
+		},]
+
 	return (
-<>               
-			<Navbar/>
-		<div className = "container">
-			<Jumbotron/>
-			
-			<div className="d-flex">
-			{cardImages.map((item,index) => {
-				return(
-					<Card key={index} img={item}/>
-				)
-			})}
+		<>
+			<Navbar />
+			<div className="container">
+				<Jumbotron />
+
+				<div className="d-flex">
+					{cardImages.map((item, index) => {
+						return (
+							<Card key={index} img={item.imagen} title={item.titulo} />
+						)
+					})}
+				</div>
+
 			</div>
-			
-		</div>
-			<Footer/>
-	</>
+			<Footer />
+		</>
 	);
 };
 
